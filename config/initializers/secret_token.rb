256e6f39c7b2faa53c4990cb49916471c885d894
Anthony Midili <tonywinslow@yahoo.com>
2013-02-25 00:34:30 -0500

    Fish Bowl initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FishBowl::Application.config.secret_token = '98282a4c1721870127f5eff17e26442d9054254161771dce6ad5e3003e0d6c665310bea19c59ae3b9ef8261187849cc8f7a2e8e5aa2c2cafef9b9f87d8727b00'
