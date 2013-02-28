e1ecdda3e81172ba2aaea718611ee1fce6901272
Anthony Midili <tonywinslow@yahoo.com>
2013-02-28 01:09:41 -0500

    Before filter set bowl variable in the test results controller.

--- app/controllers/test_results_controller.rb
+++ app/controllers/test_results_controller.rb
@@ -1,14 +1,13 @@
 class TestResultsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :set_bowl
 
   def index
-    @bowl = current_user.bowls.find(params[:bowl_id])
     @test_result = @bowl.test_results.new
     @test_results = @bowl.test_results.all
   end
 
   def create
-    @bowl = current_user.bowls.find(params[:bowl_id])
     @test_result = @bowl.test_results.new(params[:test_result])
     @test_results = @bowl.test_results.all
     if @test_result.save
@@ -19,9 +18,14 @@ class TestResultsController < ApplicationController
   end
 
   def destroy
-    @bowl = current_user.bowls.find(params[:bowl_id])
     @test_result = @bowl.test_results.find(params[:id])
     @test_result.destroy
     redirect_to bowl_test_results_path(@bowl), :notice => "Successfully destroyed test result."
+  end
+
+  private
+
+  def set_bowl
+    @bowl = current_user.bowls.find(params[:bowl_id])
   end
 end
