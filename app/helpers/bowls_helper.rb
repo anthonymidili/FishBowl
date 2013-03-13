8854116e6713911b73334fd381f3fe02f86b3f89
Anthony Midili <tonywinslow@yahoo.com>
2013-03-13 02:13:09 -0400

    Change bowl index page mock up. Add pagination to index graph(s).

--- app/helpers/bowls_helper.rb
+++ app/helpers/bowls_helper.rb
@@ -6,4 +6,9 @@ module BowlsHelper
   def default_or_avatar(bowl)
     bowl.avatar_url(:thumb) || "cool_fish_bowl.jpg"
   end
+
+  def paginate_test_results(bowl)
+    bowl.water_health_tests.page(params[:page]).per(10)
+  end
+
 end
