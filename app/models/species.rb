1ccd76d8a0c99ebcc915d1eb7f39b077f02e4157
Anthony Midili <tonywinslow@yahoo.com>
2013-03-22 20:56:41 -0400

    Show species by bowl water type. IE if bowl water type is set to a freshwater tank, only show the freshwater species.

--- app/models/species.rb
+++ app/models/species.rb
@@ -10,4 +10,8 @@ class Species < ActiveRecord::Base
   validates :info, presence: true
   validates :image, presence: true
   validates :water_type, presence: true
+
+  default_scope order: 'species.name ASC'
+
+  scope :by_bowl_water_type, lambda { |water_type| where(water_type: water_type) }
 end
