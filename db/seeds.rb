45438587c1ce3f205304fb3e734069c8bbaa4003
Anthony Midili <tonywinslow@yahoo.com>
2013-03-31 11:13:29 -0400

    More seed. Up to freshwater F's.

--- db/seeds.rb
+++ db/seeds.rb
@@ -506,3 +506,322 @@ species.update_attributes(
     }
 )
 
+species = Species.find_or_create_by_name(name: 'Calico Fantail Goldfish')
+species.update_attributes(
+    image: 'freshwater/calico_fantail_goldfish.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 8,
+    info: {
+        family: 'Cyprinidae',
+        native_to: 'Asia',
+        diet: 'Omnivore',
+        temperature: '64 - 75&deg;F',
+        care_level: 'Easy',
+        tank_size: '30+ gallons',
+        scientific_name: 'Carassius auratus',
+        info: "Has black, red, orange markings. Has a forked tail and egg shaped body. May damage live plants."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Calico Ryukin Goldfish')
+species.update_attributes(
+    image: 'freshwater/calico_ryukin_goldfish.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 8,
+    info: {
+        family: 'Cyprinidae',
+        native_to: 'Asia',
+        diet: 'Omnivore',
+        temperature: '65 - 75&deg;F',
+        care_level: 'Easy',
+        tank_size: '30+ gallons',
+        scientific_name: 'Carassius auratus',
+        info: "Ryukins have a prominent arch behind their head. Coloration provides a variety of patterns of red, orange, white, and black. Pond fish that do well in larger tanks in addition to outdoor ponds."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Calico Shubukin Goldfish')
+species.update_attributes(
+    image: 'freshwater/calico_shubunkin_goldfish.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 14,
+    info: {
+        family: 'Cyprinidae',
+        native_to: 'Asia',
+        diet: 'Omnivore',
+        temperature: '64 - 75&deg;F',
+        care_level: 'Easy',
+        tank_size: '30+ gallons',
+        scientific_name: 'Carassius auratus',
+        info: "Narrow bodied fish able to swim faster than other goldfish. Has a mottled appearance with red, black, orange, and white coloration. Similar to koi without the whiskers or barbs and a much stockier body."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Cherry Barb')
+species.update_attributes(
+    image: 'freshwater/cherry_barb.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 2,
+    info: {
+        family: 'Cyprinidae',
+        native_to: 'Sri-Lanka',
+        diet: 'Omnivore',
+        temperature: '74 - 79&deg;F',
+        care_level: 'Easy',
+        tank_size: '10+ gallons',
+        scientific_name: 'Puntius titteya/ Barbus titteya',
+        info: "Timid fish that gets its name from red coloration of males during spawning. Excellent community fish. Needs plenty of hiding places to feel safe, but also needs open space to swim."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Chinese Algae Eater')
+species.update_attributes(
+    image: 'freshwater/chinese_algae_eater.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Semi-aggressive',
+    adult_size: 9,
+    info: {
+        family: 'Gyrinocheilidae',
+        native_to: 'Asia',
+        diet: 'Herbivore',
+        temperature: '72 - 82&deg;F',
+        care_level: 'Easy',
+        tank_size: '10+ gallons',
+        scientific_name: 'Gyrinocheilus aymonieri',
+        info: "Able to attach to almost anything in a tank with its mouth. Young specimens are peaceful, while adults are territorial. Does best in an established tank with plenty of circulation in the tank."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Clown Loach')
+species.update_attributes(
+    image: 'freshwater/clown_loach.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 12,
+    info: {
+        family: 'Cobitidae',
+        native_to: 'Asia',
+        diet: 'Omnivore',
+        temperature: '72 - 82&deg;F',
+        water_parameters: 'pH 6.0-7.5, KH 8-12',
+        care_level: 'Moderate',
+        tank_size: '50+ gallons',
+        scientific_name: 'Botia macracanthus',
+        info: "Peaceful bottom dweller that may school with Tiger Barbs. Active during the day and eats both ornamental snails as well as pests. Highly sensitive to treatments containing copper or malachite green."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Dalmatian Sailfin Molly')
+species.update_attributes(
+    image: 'freshwater/dalmatian_sailfin_molly.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 6.5,
+    info: {
+        family: 'Poeciliidae',
+        native_to: 'North, Central, and South America',
+        diet: 'Omnivore',
+        temperature: '72 - 82&deg;F',
+        water_parameters: 'pH 6.0-7.5, KH 8-12',
+        care_level: 'Easy',
+        tank_size: '10+ gallons',
+        scientific_name: 'Poecilia latipinna',
+        info: "Peaceful aquarium favorite. Best kept with other peaceful fish in a well planted tank. Prefers some salinity in their water, approximately one teaspoon of aquarium salt to every two gallons."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Dwarf Gourami')
+species.update_attributes(
+    image: 'freshwater/dwarf_gourami.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 2,
+    info: {
+        family: 'Belontiidae',
+        native_to: 'India',
+        diet: 'Omnivore',
+        temperature: '72 - 82&deg;F',
+        care_level: 'Moderate',
+        tank_size: '10+ gallons',
+        scientific_name: 'Colisa sp.',
+        info: "Well suited for community tanks. does best in a well planted tank. Recommended that the tank is located in a quiet location and that frequent water changes are performed."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Electric Yellow Cichlid')
+species.update_attributes(
+    image: 'freshwater/electric_yellow_cichlid.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Aggressive',
+    adult_size: 5,
+    info: {
+        family: 'Cichlidae',
+        native_to: 'Africa - Lake Malawi',
+        diet: 'Omnivore',
+        temperature: '72 - 82&deg;F',
+        water_parameters: 'pH 7.8-8.5; KH 10-15',
+        care_level: 'Easy',
+        tank_size: '30+ gallons',
+        scientific_name: 'Labidochromis caeruleus',
+        info: "Bright yellow coloration becomes more apparent with age. Also known as a Yellow Lab. Needs plenty of hiding places as well as open swimming areas."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Elephant Ear Halfmoon Plakat Betta')
+species.update_attributes(
+    image: 'freshwater/elephant_ear_halfmoon_plakat_betta.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 3,
+    info: {
+        family: 'Belontiidae',
+        native_to: 'S.E. Asia - Cambodia, Thailand',
+        diet: 'Carnivore',
+        temperature: '74 - 82&deg;F',
+        water_parameters: 'pH 6.0-8.0; KH 0-25',
+        care_level: 'Easy',
+        tank_size: '1+ liter',
+        scientific_name: 'Betta splendens',
+        info: "Comes in a wide variety of colors. Can breathe from the surface due to modified gills, known as the labyrinth organ. Male bettas should never be housed together for any reason."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Elongated Mbuna')
+species.update_attributes(
+    image: 'freshwater/elongated_mbuna.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Aggressive',
+    adult_size: 6,
+    info: {
+        family: 'Cichlidae',
+        native_to: 'Africa - Lake Malawi',
+        diet: 'Omnivore',
+        temperature: '72 - 82&deg;F',
+        care_level: 'Easy',
+        tank_size: '30+ gallons',
+        scientific_name: 'Pseudotropheus elongatus',
+        info: "Has a longer body than most Pseudotropheus cichlids. One of the more aggressive cichlids. Prefers plenty of hiding spots as well as open space for swimming."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Crowntail Betta - Female')
+species.update_attributes(
+    image: 'freshwater/female_crowntail_betta.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 3,
+    info: {
+        family: 'Belontiidae',
+        native_to: 'S.E. Asia - Cambodia, Thailand',
+        diet: 'Carnivore',
+        water_current: 'Low',
+        temperature: '74 - 82&deg;F',
+        water_parameters: 'pH 6.0-8.0, KH 0-25',
+        care_level: 'Easy',
+        tank_size: '1+ liter',
+        scientific_name: 'Betta splendens',
+        info: "Comes in a wide variety of colors. Can breathe from the surface due to modified gills, known as the labyrinth organ. Crown Tail Bettas have stunning tails with long fringes."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Deltatail Betta - Female')
+species.update_attributes(
+    image: 'freshwater/female_deltatail_betta.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 3,
+    info: {
+        family: 'Belontiidae',
+        native_to: 'S.E. Asia - Cambodia, Thailand',
+        diet: 'Carnivore',
+        water_current: 'Low',
+        temperature: '74 - 82&deg;F',
+        water_parameters: 'pH 6.0-8.0, KH 0-25',
+        care_level: 'Easy',
+        tank_size: '1+ liter',
+        scientific_name: 'Betta splendens',
+        info: "Comes in a wide variety of colors. Can breathe from the surface due to modified gills, known as the labyrinth organ. Delta Tail Bettas have tails in the shape of a splendid triangle."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Halfmoon Betta - Female')
+species.update_attributes(
+    image: 'freshwater/female_halfmoon_betta.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 3,
+    info: {
+        family: 'Belontiidae',
+        native_to: 'S.E. Asia - Cambodia, Thailand',
+        diet: 'Carnivore',
+        water_current: 'Low',
+        temperature: '74 - 82&deg;F',
+        water_parameters: 'pH 6.0-8.0, KH 0-25',
+        care_level: 'Easy',
+        tank_size: '1+ liter',
+        scientific_name: 'Betta splendens',
+        info: "Comes in a wide variety of colors. Can breathe from the surface due to modified gills, known as the labyrinth organ. Halfmoon Bettas have a fragile tail in the shape of a beautiful halfmoon."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Veiltail Betta - Female')
+species.update_attributes(
+    image: 'freshwater/female_veiltail_betta.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Community',
+    adult_size: 3,
+    info: {
+        family: 'Belontiidae',
+        native_to: 'S.E. Asia - Cambodia, Thailand',
+        diet: 'Carnivore',
+        water_current: 'Low',
+        temperature: '74 - 82&deg;F',
+        water_parameters: 'pH 6.0-8.0, KH 0-25',
+        care_level: 'Easy',
+        tank_size: '1+ liter',
+        scientific_name: 'Betta splendens',
+        info: "Attractive tail shape, similar to a wedding veil. Can breathe from the surface due to modified gills, known as the labyrinth organ. Female Bettas have short, but colorful fins."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Fire Eel')
+species.update_attributes(
+    image: 'freshwater/fire_eel.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Semi-Aggressive',
+    adult_size: 24,
+    info: {
+        family: 'Mastacembelidae',
+        native_to: 'South East Asia',
+        diet: 'Carnivore',
+        temperature: '72 - 82&deg;F',
+        care_level: 'Moderate',
+        tank_size: '50+ gallons',
+        scientific_name: 'Mastacembelus erythoraenia',
+        info: "Not a true eel, but an elongated fish with spines. Needs fine, non-abrasive substrate for burrowing. Does best as the only Fire Eel in a tank, and needs a tight fitting lid to prevent escape."
+    }
+)
+
+species = Species.find_or_create_by_name(name: 'Firemouth Cuchlid')
+species.update_attributes(
+    image: 'freshwater/firemouth_cuchlid.jpg',
+    water_type: 'Freshwater',
+    temperament: 'Semi-Aggressive',
+    adult_size: 6,
+    info: {
+        family: 'Cichlidae',
+        native_to: 'Central & South America',
+        diet: 'Carnivore',
+        temperature: '72 - 82&deg;F',
+        water_parameters: 'pH 6.0-7.8; KH 4-10',
+        care_level: 'Moderate',
+        tank_size: '30+ gallons',
+        scientific_name: 'Cichlasoma meeki',
+        info: "Territorial fish that needs areas designated by piles of rocks. Beautiful cichlid with a bright red throat and underbelly. Live plants may be used as hiding places or territories only when using hardy plants."
+    }
+)
+
