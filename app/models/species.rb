5a429007fab450259e6587f43e8be8efc4c509f3
Anthony Midili <tonywinslow@yahoo.com>
2013-03-30 22:43:57 -0400

    Bugs fixed with collection_select scopes. Thanks Frankie. You make it look too easy.

--- app/models/species.rb
+++ app/models/species.rb
@@ -16,5 +16,5 @@ class Species < ActiveRecord::Base
   default_scope order: 'species.name ASC'
 
   scope :by_bowl_water_type, lambda { |water_type| where('water_type = ?', water_type) }
-  scope :all_not_shown, lambda { |species_id| where('id not IN (?)', species_id) }
+  scope :all_but_shown, lambda { |species_ids| where('id NOT IN (?)', (species_ids.blank? ? '' : species_ids)) }
 end
