5bf20950033801a7697d88833102ae3d00075bf2
Anthony Midili <tonywinslow@yahoo.com>
2013-04-04 22:23:41 -0400

    Damn you postgresql!!!

--- app/models/species.rb
+++ app/models/species.rb
@@ -17,7 +17,7 @@ class Species < ActiveRecord::Base
 
   default_scope order: 'species.name ASC'
 
-  scope :by_bowl_water_type, lambda { |water_type| where('water_type = (?)', water_type) }
+  scope :by_bowl_water_type, lambda { |water_type| where('water_type = ?', water_type) }
   scope :by_custom_species, lambda { |user_id| where('created_by_id IS NULL OR created_by_id IS (?)', user_id) }
   scope :all_but_shown, lambda { |species_ids| where('id NOT IN (?)', (species_ids.blank? ? 0 : species_ids)) }
 end
