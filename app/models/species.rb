b9970d2cb879e05f97bcb145dd5511bf599069ae
Anthony Midili <tonywinslow@yahoo.com>
2013-04-08 22:42:35 -0400

    Improved scopes.

--- app/models/species.rb
+++ app/models/species.rb
@@ -21,5 +21,5 @@ class Species < ActiveRecord::Base
   scope :custom_for, lambda { |user_id| where(created_by_id: user_id) }
   scope :all_available_to, lambda { |user_id| where(created_by_id: [ user_id, nil ]) }
   scope :by_bowl_water_type, lambda { |water_type| where(water_type: water_type) }
-  scope :all_but_shown, lambda { |species_ids| where('id NOT IN (?)', (species_ids.blank? ? 0 : species_ids)) }
+  scope :all_but_shown, lambda { |species_ids| Species.where(Arel::Table.new(:species)[:id].not_in([*species_ids, 0])) }
 end
