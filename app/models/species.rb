78a48d9c3c1c1643d2960f50d76b972d0fe79122
Anthony Midili <tonywinslow@yahoo.com>
2013-04-20 20:28:56 -0400

    Improved species form with improved carrierwave.

--- app/models/species.rb
+++ app/models/species.rb
@@ -1,5 +1,5 @@
 class Species < ActiveRecord::Base
-  attr_accessible :name, :image, :avatar, :remove_avatar, :water_type, :temperament, :adult_size, :info, :reef_safe, :family, :native_to, :diet, :temperature, :care_level, :tank_size, :scientific_name, :water_current, :water_parameters, :about, :created_by_id
+  attr_accessible :name, :image, :avatar, :avatar_cache, :remove_avatar, :water_type, :temperament, :adult_size, :info, :reef_safe, :family, :native_to, :diet, :temperature, :care_level, :tank_size, :scientific_name, :water_current, :water_parameters, :about, :created_by_id
 
   has_many :occupancies, dependent: :destroy
   has_many :bowls, through: :occupancies
