f22f6b42acf40720c7ed575cc79bad9dc7c4a180
Anthony Midili <tonywinslow@yahoo.com>
2013-04-28 16:43:02 -0400

    Protect Admin Controller from users that are not admins.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,13 @@
 class AdminController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :admin_user
+
   def administrator
+  end
+
+private
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
