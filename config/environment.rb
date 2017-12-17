0ca5850f9395c1497e9630f23c0874b02c01a00b
AMidili <tonywinslow@yahoo.com>
2017-12-17 09:55:50 -0500

    Update devise views. Use sendgrid for development mailer.

--- config/environment.rb
+++ config/environment.rb
@@ -2,13 +2,14 @@
 require_relative 'application'
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["SENDGRID_USERNAME"],
-  :password => ENV["SENDGRID_PASSWORD"],
-  :domain => "fish-bowl.herokuapp.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
+  user_name:            ENV["SENDGRID_USERNAME"],
+  password:             ENV["SENDGRID_PASSWORD"],
+  address:              ENV['SENDGRID_ADDRESS'],
+  domain:               ENV['SMTP_DOMAIN'],
+  port:                 ENV['SMTP_PORT'],
+  authentication:       ENV['SMTP_AUTHENTICATION'],
+  openssl_verify_mode:  ENV['SMTP_OPENSSL_VERIFY_MODE'],
+  enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO']
 }
 
 # Initialize the Rails application.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -53,16 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Devise mailer
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => ENV['DEFAULT_URL_OPTIONS'] }
 
   config.action_mailer.delivery_method = :smtp
-
-  ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.gmail.com",
-    :port           => 587,
-    :authentication => :plain,
-    :user_name      => "tonywinslow13@gmail.com",
-    :password       => "1taylorguitar",
-    :enable_starttls_auto => true
-  }
 end
